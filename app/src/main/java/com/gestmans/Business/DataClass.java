b82f69869d41a76f01ab1cd1ae2e41a14d33a771
ericbargim <ericbargim@users.noreply.github.com>
2020-05-11 20:07:27 +0200

    [HOME] Created XML.

--- app/src/main/java/com/gestmans/Business/DataClass.java
+++ app/src/main/java/com/gestmans/Business/DataClass.java
@@ -2,4 +2,5 @@ package com.gestmans.Business;
 
 public class DataClass {
     public static String password;
+    public static String username;
 }
---
+++ app/src/main/java/com/gestmans/Interface/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.gestmans.Interface;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.gestmans.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
